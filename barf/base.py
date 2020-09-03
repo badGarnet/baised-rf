9b430e9cd27d9908a2860976bf95b4a91e6ad352
Yao You <theyaoyou@gmail.com>
2020-09-03 04:53:15 -0500

    implement baseestimator class

--- barf/base.py
+++ barf/base.py
@@ -4,9 +4,46 @@ import logging
 log = logging.getLogger(name=__name__)
 
 # base class definition for estimator
+
+
 class BaseEstimator:
-    def get_params(self, deep=False):
+    def __init__(self):
+        self.name = 'BaseEstimator'
+
+    @staticmethod
+    def _parse_params(parms):
         return NotImplemented
+
+    @staticmethod
+    def _check_value(value, deep=False):
+        if isinstance(value, BaseEstimator):
+            return value.get_params(deep)
+        else:
+            return value
+
+    @staticmethod
+    def _flatten_param_dict(param_dict, collection, prefix=None):
+        for key, value in param_dict.items():
+            if isinstance(value, dict):
+                collection = BaseEstimator._flatten_param_dict(value, collection, key)
+            else:
+                if prefix is None:
+                    collection[key] = value
+                else:
+                    collection['__'.join([prefix, key])] = value
+        return collection
+
+    def get_params(self, deep=False):
+        results = dict()
+        for key, value in self.__dict__.items():
+            if key.startswith('_'):
+                if deep:
+                    results[key] = self._check_value(value, deep)
+            else:
+                results[key] = self._check_value(value)
+        
+        flattened = dict()
+        return self._flatten_param_dict(results, flattened)
 
     def set_params(self, **kwargs):
         for key, value in kwargs.items():
--- tests/test_base.py
+++ tests/test_base.py
@@ -4,5 +4,78 @@ from barf.base import BaseEstimator
 
 class TestEstimator(unittest.TestCase):
     def test_init(self):
-        estimator = BaseEstimator
+        estimator = BaseEstimator()
         self.assertEqual('BaseEstimator', estimator.__class__.__name__)
+
+    def test_get_params(self):
+        estimator = BaseEstimator()
+        params = estimator.get_params()
+        self.assertEqual({'name': 'BaseEstimator'}, params)
+
+    def test_check_value(self):
+        a = 'a'
+        self.assertEqual(a, BaseEstimator._check_value(a))
+
+    def test_flatten_param_dict(self):
+        a = {'a': 'a', 'b': {'a': 'b1', 'b': 'b2'}}
+        b = {'a': 'a', 'b__a': 'b1', 'b__b': 'b2'}
+        flattened = dict()
+        flattened = BaseEstimator()._flatten_param_dict(a, flattened, prefix=None)
+        self.assertDictEqual(
+            b, flattened
+        )
+
+    def test_check_value_with_estimator(self):
+        a = BaseEstimator()
+        self.assertEqual({'name': 'BaseEstimator'}, BaseEstimator._check_value(a))
+
+    def test_set_params(self):
+        a = BaseEstimator()
+        a.set_params(name='new')
+        self.assertEqual('new', a.name)
+
+    def test_set_params_not_found(self):
+        a = BaseEstimator()
+        a.set_params(foo='new')
+        self.assertFalse(hasattr(a, 'foo'))
+
+    def test_deep_params_false(self):
+        class NewEstimator(BaseEstimator):
+            def __init__(self, foo):
+                self.estimator = BaseEstimator()
+                self.foo = foo
+                self.name = 'NewEstimator'
+                self._hidden = 'secret'
+
+        a = NewEstimator('foo')
+        self.assertEqual(
+            {'foo': 'foo', 'estimator__name': 'BaseEstimator', 'name': 'NewEstimator'},
+            a.get_params()
+        )
+
+    def test_deep_params_true(self):
+        class NewEstimator(BaseEstimator):
+            def __init__(self, foo):
+                self.estimator = BaseEstimator()
+                self.foo = foo
+                self.name = 'NewEstimator'
+                self._hidden = 'secret'
+
+        a = NewEstimator('foo')
+        self.assertEqual(
+            {'foo': 'foo', 'estimator__name': 'BaseEstimator', 'name': 'NewEstimator', '_hidden': 'secret'},
+            a.get_params(deep=True)
+        )
+
+    def test_nesting_estimator(self):
+        class NewEstimator(BaseEstimator):
+            def __init__(self, foo):
+                self.estimator = BaseEstimator()
+                self.foo = foo
+                self.name = 'NewEstimator'
+
+        a = NewEstimator('foo')
+        self.assertEqual(
+            {'foo': 'foo', 'estimator__name': 'BaseEstimator', 'name': 'NewEstimator'},
+            a.get_params()
+        )
